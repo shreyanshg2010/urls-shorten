105908e8f2d779e17a3fe55907edf69fb9c658df
Shreyansh Goyal <shreyanshgoyal@Shreyanshs-MacBook-Pro.local>
2021-10-31 17:52:40 +0530

    Initial commit with implementation of urls

---
+++ pages/api/redirect.ts
@@ -0,0 +1,41 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { MongoClient, ObjectId } from "mongodb";
+import { NowRequest, NowResponse } from "@vercel/node";
+
+let cachedDb;
+
+async function connectToDb() {
+  const { MongoClient } = require("mongodb");
+  if (cachedDb) return cachedDb;
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+  const client = new MongoClient(process.env.MONGODB_URI, options);
+  cachedDb = client;
+  return await client.connect();
+}
+
+export default async (req: NowRequest, res: NowResponse) => {
+  const db = await connectToDb();
+  const entry = await db
+    .db("links_db")
+    .collection("links_collection")
+    .findOne({ _id: new ObjectId(req.query.id as string) });
+  if(entry !==null){
+      return res.redirect(301, entry.link);
+  } 
+  return res.redirect(301, '/'entry);
+
+  console.log("req", req.body.link);
+  if (req.body.link) {
+    res.statusCode = 201;
+    return res.json({
+      short_link: `https://localhost:3000/r/${entry.insertedId}`,
+    });
+  }
+
+  res
+    .status(409)
+    .json({ error: "no_link_found", error_description: "No link found" });
+};
---
+++ pages/api/shorten_link.ts
@@ -0,0 +1,35 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { MongoClient } from "mongodb";
+import { NowRequest, NowResponse } from "@vercel/node";
+
+let cachedDb;
+
+async function connectToDb() {
+  const { MongoClient } = require("mongodb");
+  if (cachedDb) return cachedDb;
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+  const client = new MongoClient(process.env.MONGODB_URI, options);
+  cachedDb = client;
+  return await client.connect();
+}
+
+export default async (req: NowRequest, res: NowResponse) => {
+  const db = await connectToDb();
+
+  console.log("req", req.body.link);
+  if (req.body.link) {
+    const entry = await db
+      .db("links_db")
+      .collection("links_collection")
+      .insertOne({ link: req.body.link });
+    res.statusCode = 201;
+    return res.json({ short_link: `https://localhost:3000/r/${entry.insertedId}` });
+  }
+
+  res
+    .status(409)
+    .json({ error: "no_link_found", error_description: "No link found" });
+};
