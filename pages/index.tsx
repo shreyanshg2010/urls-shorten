d43d47b66612005bc6d0bc8dad2b53242374e619
Shreyansh Goyal <shreyanshgoyal@Shreyanshs-MacBook-Pro.local>
2021-11-02 14:54:30 +0530

    changes added for 6 digits

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,7 @@ export default function Home() {
       setMessage(response.data?.short_link);
     } catch (e) {
       const error = e as AxiosError<ShortenLinkError>;
-      console.log(error);
+      console.log(e);
       setStatus("error");
       setMessage(
         error.response?.data?.error_description || "Something went wrong"
